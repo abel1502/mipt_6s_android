adba00f32d9c7852975d4e569d8f6c5ee196f828
abel1502 <terragame.abel@gmail.com>
2023-03-21 22:40:20 +0300

    Task1 more minor updates

--- app/src/main/java/com/abel/mipt_6s_test_app/Task1.kt
+++ app/src/main/java/com/abel/mipt_6s_test_app/Task1.kt
@@ -18,12 +18,15 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -104,6 +107,7 @@ fun Task1() {
                 fontWeight = FontWeight(400),
                 lineHeight = 19.98.sp,
                 color = Color(0xFF53E88B),
+                textDecoration = TextDecoration.Underline,
             )
         }
         Task1Background()
